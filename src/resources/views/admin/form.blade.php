24d151ff28a76da1233996efc62b648ed5149ee8
Philip Sørensen <git@ppsa.dk>
2024-11-30 21:07:40 +0100

    Added value to checkbox.

--- src/resources/views/admin/form.blade.php
+++ src/resources/views/admin/form.blade.php
@@ -7,7 +7,7 @@
 		<x-formcomponents::input col="col-sm-6" id="event_end" :name="trans('crudevents::event.timeend')" :value="old('event_start', isset($event) ? date('Y-m-d H:i', strtotime($event->event_end)) : '')" />
 		<x-formcomponents::image :name="isset($event) ? trans('crudevents::event.image.replace') : trans('crudevents::event.image')" />
 		<x-formcomponents::input col="col-sm-6" id="location" :name="trans('crudevents::event.location')" :value="old('location', isset($event) ? $event->location : '')" />
-		<x-formcomponents::checkbox col="col-sm-6 align-self-center" id="public" :name="trans('crudevents::event.public')" />
+		<x-formcomponents::checkbox :checked="isset($event) ? $event->public : false" col="col-sm-6 align-self-center" id="public" :name="trans('crudevents::event.public')" />
 		<x-formcomponents::input id="description" :name="trans('crudevents::event.description')" :value="old('description', isset($event) ? $event->description : '')" />
 		<x-formcomponents::text id="text" :name="trans('crudevents::event.text')" :value="old('text', isset($event) ? $event->text : '')" />
 		<x-formcomponents::button onclick="summernoteSubmit();">
